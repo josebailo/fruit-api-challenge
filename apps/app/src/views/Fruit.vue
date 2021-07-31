25ba460926fd0b4b574964d07fa125649685b114
Jose Bailo <jmbailo@gmail.com>
2021-07-31 18:29:30 +0100

    Create BackButton component

--- apps/app/src/views/Fruit.vue
+++ apps/app/src/views/Fruit.vue
@@ -1,11 +1,6 @@
 <template>
   <div>
-    <router-link :to="{ name: 'Home' }" class="inline-flex items-center text-sm hover:text-gray-600">
-      <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 17l-5-5m0 0l5-5m-5 5h12" />
-      </svg>
-      <span>Go back</span>
-    </router-link>
+    <BackButton />
     <div class="mt-5">
       <FruitCard v-if="fruit" :fruit="fruit" />
     </div>
@@ -13,11 +8,13 @@
 </template>
 
 <script>
+import BackButton from '@/components/BackButton'
 import FruitCard from '@/components/FruitCard'
 
 export default {
   name: 'Fruit',
   components: {
+    BackButton,
     FruitCard
   },
   computed: {
